3a45f56c1c13dd64803ceef5ffaa73801d04daae
Slaven B <slaven.bunijevac@gmail.com>
2021-12-30 22:13:22 +0100

    Introduce Navbar component

---
+++ src/layouts/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      
+      <ul>
+        <li>Slaven</li>
+        <li>Slaven</li>
+        <li>Slaven</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
