650e34f570724140384180200da7581928dcf5ca
Slaven B <slaven.bunijevac@gmail.com>
2021-12-31 07:24:18 +0100

    introduce questions and users components

---
+++ src/components/Users.js
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <div>
+      <h3>Users Component</h3>
+    </div>
+  )
+}
+
+export default Users
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,12 @@
+import Questions from '../components/Questions';
+import Users from '../components/Users';
+
 const Homepage = () => {
   return (
     <div>
       <h1>Ovo je Slavenov Homepage</h1>
+      <Questions />
+      <Users />
     </div>
   )
 }
