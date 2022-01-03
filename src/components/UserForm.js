8a5b4bbd8c3d4a4ddf299c7c4452d0ff626b7ea8
Slaven B <slaven.bunijevac@gmail.com>
2022-01-03 12:46:15 +0100

    introduce form input handler

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,17 +1,65 @@
+import { useEffect, useState } from 'react';
+
 const UserForm = () => {
+
+  const [name, setName ] = useState();
+  const [email, setEmail ] = useState();
+  const [password, setPassword ] = useState();
+
+  const inputHandler = e => {
+    const value = e.target.value;
+    const targetElement = e.target.name;
+    switch (targetElement) {
+      case 'name':
+        setName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      default:
+        setPassword(value);
+    }
+  }
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+    console.log(name, email, password);
+  }
+  
   return (
-    <form className="col-sm-8 col-md-6">
+    <form className="col-sm-8 col-md-6" onSubmit={formSubmitHandler}>
       <div className="mb-3 text-start">
         <label htmlFor="name" className="px-0 form-control-lg col-form-label">Name</label>
-        <input type="text" id="name" placeholder="Enter your name..." className="form-control" />
+        <input 
+          onChange={inputHandler}
+          value={name || ''}
+          name="name"
+          type="text" 
+          id="name" 
+          placeholder="Enter your name..." 
+          className="form-control" />
       </div>
       <div className="mb-3 text-start">
         <label htmlFor="email" className="px-0 form-control-lg col-form-label">Email</label>
-        <input type="email" id="email" placeholder="Enter your email..." className="form-control" />
+        <input 
+          onChange={inputHandler}
+          value={email || ''}
+          name="email"
+          type="email" 
+          id="email" 
+          placeholder="Enter your email..." 
+          className="form-control" />
       </div>
       <div className="mb-3 text-start">
         <label htmlFor="password" className="px-0 form-control-lg col-form-label">Password</label>
-        <input type="password" id="password" placeholder="Enter your password..." className="form-control" />
+        <input
+          value={password || ''}
+          onChange={inputHandler}
+          name="password"
+          type="password" 
+          id="password" 
+          placeholder="Enter your password..." 
+          className="form-control" />
       </div>
       <div className="col-6 text-start">
         <button type="submit" className="my-3 col-6 btn btn-primary">Sign in</button>
