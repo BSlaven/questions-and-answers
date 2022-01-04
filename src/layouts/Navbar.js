60a2c5391f4fa452158baa8cfb08b3ec342a9a77
Slaven B <slaven.bunijevac@gmail.com>
2022-01-04 21:32:50 +0100

    introduce logout button to test logout functionality

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,24 +1,30 @@
 import { Link } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 const MainNav = ({ user }) => {
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const logoutUser = () => {
     console.log('User logged out');
-    // dispatch({ type: 'LOGOUT_USER '});
+    dispatch({ type: 'LOGOUT_USER'});
   }
   
   return (
-    <nav className="navbar navbar-dark bg-dark p-3 mb-3 navbar-expand-sm d-flex align-items-center">      
+    <nav className="navbar navbar-dark bg-dark p-3 mb-3 navbar-expand-sm d-flex align-items-center">
       <Link to="/" className="navbar-brand text-white text-decoration-none">Logo</Link>
+      <button 
+          type='button'
+          className="ms-3 btn btn-outline-danger"
+          onClick={logoutUser}>
+        logout
+      </button>
       <button type='button' className="navbar-toggler" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span className='navbar-toggler-icon'></span>
       </button>
       
       <div id="main-navbar" className="collapse navbar-collapse justify-content-end">
-        {!user ? <ul className='navbar-nav mr-auto'>
+        {/* {!user ? <ul className='navbar-nav mr-auto'>
           <li className='mx-2 px-3 nav-item bg-success p-2 rounded'>
             <Link to="/login" className="text-light text-decoration-none">login</Link>
           </li>
@@ -28,7 +34,7 @@ const MainNav = ({ user }) => {
         </ul> : <button 
           type='button'
           className="btn btn-outline-danger"
-          onClick={logoutUser}>logout</button> }
+          onClick={logoutUser}>logout</button> } */}
       </div>
     </nav>
   )
