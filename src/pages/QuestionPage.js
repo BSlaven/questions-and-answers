81051ce926a08961a568556fc9426dac46fb521c
Slaven B <slaven.bunijevac@gmail.com>
2022-01-09 14:38:38 +0100

    introduce one question page

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const QuestionPage = () => {
   return (
-    <div>
-      <h4>Ovo je question stranica</h4>
+    <div style={{ color: "#1C2833" }} className="container">
+      <div className="row mt-5 col-sm-12 col-lg-8">
+        <p className="fs-2 col-sm- text-center">Ovo je tekst pitanja</p>
+      </div>
     </div>
   )
 }
