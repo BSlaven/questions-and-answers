1fe62b21fa05d0d7645ae74ba68c7dca3d4e3c11
Slaven B <slaven.bunijevac@gmail.com>
2022-01-12 10:53:05 +0100

    add number of questions for user

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
 
 const User = ({ user }) => {
+
+  const questions = useSelector(state => state.questions);
+  const questionsThisUser = questions.filter(q => q.author === user.userId);
 
   return (
     <div className="card my-2">
       <div className="card-body text-secondary">
-        {`${user.name || user.email} (${user.questions.length})`}
+        {`${user.name || user.email} (${questionsThisUser.length})`}
       </div>
     </div>
   )
